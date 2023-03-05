23f10112e0b7dbdd992a2c22707bbd5f0b04b93c
Evangelos Tsougiannis <evangelos.tsougiannis@cibos.gr>
2023-03-05 13:24:12 +0200

    Update project Ids and bucket

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "gifted-cooler-378519-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="gifted-cooler-378519"
